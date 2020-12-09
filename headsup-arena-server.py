5fee7d779478c5bdd6bf19cc36ff32a5e47d1461
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-09 16:52:10 -0500

    Init repo

---
+++ headsup-arena-server.py
@@ -0,0 +1,34 @@
+import arena
+import json
+import random
+
+
+# TODO : investigate whether GM can give input from this callback...
+def scene_callback(msg):
+  jsonMsg = json.loads(msg)
+
+
+
+arena.init(
+  "arena.andrew.cmu.edu",
+  "realm",
+  "heads-up-testing",
+  scene_callback
+)
+
+# TODO : Make list of all (ARENA) Users
+
+# TODO :
+
+
+# Create objects
+sceneLight = arena.Object(
+  objName="a-sceneLight",
+  objType=arena.Shape.light,
+  location=(0, 50, -115),
+  persist=True,
+)
+
+
+
+arena.handle_events()
