1b1443d586ae0984225885f9e5457e440c89dc69
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-16 16:26:37 -0500

    Attach test sphere to static name cam

---
+++ del-test-ui.py
@@ -0,0 +1,14 @@
+import arena
+
+
+
+arena.init(
+  "arena.andrew.cmu.edu",
+  "realm",
+  "headsup-dev-test"
+)
+
+
+
+testUI = arena.Object(objName="a-testUI")
+testUI.delete()
--- headsup-server.py
+++ headsup-server.py
@@ -3,25 +3,27 @@ import json
 import random
 
 
-# TODO : investigate whether GM can give input from this callback...
+
+# Catches all scene-specific MQTT messages (I think) ...
 def scene_callback(msg):
+  print( msg )
   jsonMsg = json.loads(msg)
+
+# TODO : create function to add UI to user cam
 
 
 
 arena.init(
   "arena.andrew.cmu.edu",
   "realm",
-  "heads-up-testing",
+  "headsup-dev-test",
   scene_callback
 )
 
 
-# TODO : create minigame HUD
-
-
 
 # Create objects
+## Light is typically useful --v
 sceneLight = arena.Object(
   objName="a-sceneLight",
   objType=arena.Shape.light,
@@ -29,6 +31,16 @@ sceneLight = arena.Object(
   persist=True,
 )
 
+# test: create test object mounted to user cam
+testUI = arena.Object(
+  objName="a-testUI",
+  objType=arena.Shape.circle,
+  parent="camera_64_LukeJones",
+  location=(-0.5, 0, -0.5),
+  #scale=(0.1, 0.1, 0.1),
+)
 
 
+
+# Listens for relevant events --v
 arena.handle_events()
