040dd58fd714314b12ff44d90a3e6092d46ea80f
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-16 16:54:35 -0500

    Add mock UI to every camera that joins scene

--- headsup-server.py
+++ headsup-server.py
@@ -1,6 +1,13 @@
 import arena
 import json
+import re
+
 import random
+
+
+
+# Global vars --v
+headsup_ui_cams = list()
 
 
 
@@ -9,7 +16,26 @@ def scene_callback(msg):
   print( msg )
   jsonMsg = json.loads(msg)
 
-# TODO : create function to add UI to user cam
+  if re.match("camera_.*", jsonMsg["object_id"]):
+    cam_id = jsonMsg["object_id"]
+    print(cam_id + " matches!")
+    if headsup_ui_cams.count(cam_id) == 0:
+      print(cam_id + " added to list of all user cams")
+      headsup_ui_cams.append(cam_id)
+      # Add headsup ui to newly found cam
+      new_headsup_ui(cam_id)
+
+
+# Adds testUI to dynamically acquired user cam ---v
+def new_headsup_ui(cam_object_id):
+  # test: create test object mounted to user cam
+  testUI = arena.Object(
+    objName="a-testUI",
+    objType=arena.Shape.circle,
+    parent=cam_object_id,
+    location=(-0.5, 0, -0.5),
+    scale=(0.1, 0.1, 0.1),
+  )
 
 
 
@@ -21,25 +47,14 @@ arena.init(
 )
 
 
-
-# Create objects
-## Light is typically useful --v
+# Create objects ...
+## ... Light is typically useful --v
 sceneLight = arena.Object(
   objName="a-sceneLight",
   objType=arena.Shape.light,
   location=(0, 50, -115),
   persist=True,
 )
-
-# test: create test object mounted to user cam
-testUI = arena.Object(
-  objName="a-testUI",
-  objType=arena.Shape.circle,
-  parent="camera_64_LukeJones",
-  location=(-0.5, 0, -0.5),
-  #scale=(0.1, 0.1, 0.1),
-)
-
 
 
 # Listens for relevant events --v
