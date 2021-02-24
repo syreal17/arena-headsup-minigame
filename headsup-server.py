99a7975bcf0f3488f12c11623222d88333beb04c
syreal17 <ltjones@acm.org>
2021-02-24 14:17:42 -0500

    Add possible camera-UI button

--- headsup-server.py
+++ headsup-server.py
@@ -23,8 +23,16 @@ def user_join_headsup(camera):
 
 # OTHER DEFS ------------------------------------------------------------------
 
-def mount_headsup_ui(object_id):
-   print("Test mount headsup ui to " + object_id)
+def mount_headsup_ui(parent_id):
+   print("Test mount headsup ui to " + parent_id)
+   button_test1 = Triangle( object_id = parent_id + "__button_test1",
+                          parent = parent_id,
+                          position = Position(-0.1, 0.0, -0.25),
+                          scale = Scale(0.03, 0.03, 0.03),
+                          material = Material(color=Color(255, 255, 0), opacity=0.3)
+                        )
+
+   scene.add_object(button_test1)
 
 
 
@@ -32,11 +40,12 @@ def mount_headsup_ui(object_id):
 
 @scene.run_once
 def main():
-   box1 = Box( object_id="a-box1",
-               material=Material(color=Color(255, 100, 255)),
-               position=Position(0, 0, 0),
-               scale=Scale(1, 1, 1),
-               persist=False )
+   box1 = Box( object_id = "a-box1",
+               material = Material(color=Color(255, 100, 255)),
+               position = Position(0, 0, 0),
+               scale = Scale(1, 1, 1),
+               persist = False
+             )
 
    scene.add_object(box1)
 
