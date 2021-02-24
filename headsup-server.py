94ff9a5987089b839f89a699683bb0c577536ca7
syreal17 <ltjones@acm.org>
2021-02-24 11:05:21 -0500

    Establish correct function flow

--- headsup-server.py
+++ headsup-server.py
@@ -10,55 +10,46 @@ from arena import *
 
 headsup_ui_cams = list()
 
-arena = Arena()
+scene = Arena()
 
 
 
+# CALLBACKS -------------------------------------------------------------------
+
+def user_join_headsup(camera):
+   mount_headsup_ui(camera.object_id)
 
 
-# TODO : update for 0.1.0 refactor --v
-#        Catches all scene-specific MQTT messages
-#def scene_callback(msg):
-#  print( msg )
-#  jsonMsg = json.loads(msg)
-#
-#  if re.match("camera_.*", jsonMsg["object_id"]):
-#    cam_id = jsonMsg["object_id"]
-#    print(cam_id + " matches!")
-#    if headsup_ui_cams.count(cam_id) == 0:
-#      print(cam_id + " added to list of all user cams")
-#      headsup_ui_cams.append(cam_id)
-#      # Add headsup ui to newly found cam
-#      new_headsup_ui(cam_id)
 
-# TODO : update function for 0.1.0 refactor ---v
-# Adds testUI to dynamically acquired user cam ---v
-#def new_headsup_ui(cam_object_id):
-#  # test: create test object mounted to user cam
-#  testUI = arena.Object(
-#    objName="a-testUI",
-#    objType=arena.Shape.circle,
-#    parent=cam_object_id,
-#    location=(-0.5, 0, -0.5),
-#    scale=(0.1, 0.1, 0.1),
-#  )
+# OTHER DEFS ------------------------------------------------------------------
+
+def mount_headsup_ui(object_id):
+   print("Test mount headsup ui to " + object_id)
 
 
 
 # MAIN ------------------------------------------------------------------------
 
-@arena.run_once
+@scene.run_once
 def main():
-    box1 = Box( object_id="a-box1",
-                 color=Color(255, 100, 255),
-                 position=Position(0, 0, 0),
-                 scale=Scale(1, 1, 1),
-                 persist=False )
-    arena.add_object(box1)
+   box1 = Box( object_id="a-box1",
+               material=Material(color=Color(255, 100, 255)),
+               position=Position(0, 0, 0),
+               scale=Scale(1, 1, 1),
+               persist=False )
+
+   scene.add_object(box1)
+
+   # Add UI to existing users
+   for user in scene.users:
+      mount_headsup_ui(user.object_id)
+
+   # Callback to add UI to future users who join scene
+   scene.user_join_callback = user_join_headsup
 
 
 
 # THREADS ---------------------------------------------------------------------
 
-arena.run_tasks()
+scene.run_tasks()
 
